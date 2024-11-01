fbff65ceeba34a5b8edad015f74c19a8e6c9c5fe
Samriddhiy <samriddhi4005@gmail.com>
2024-11-01 20:40:48 +0530

    login made error corrected

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,14 +2,14 @@
 
 const asyncHandler = (requestHandler) => {
     return (req, res, next )=> {
-        Promise.resolve((req, res, next)).
-        catch((err) => next(err))
-    }
-}
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err));
+    };
+};
 
 
 
-export { asyncHandler}
+export { asyncHandler};
 
 
 // This one is of try catch 
